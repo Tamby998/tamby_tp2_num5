cdbd37d191bf302d426af2db1b8b9f08cf923c60
Tamby998 <tambymampioninaheriharson@gmail.com>
2022-04-04 08:30:05 +0300

    Tp2 Tamby Numero 5

---
+++ src/main/java/com/itu/tp2_nicole_num12/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itu.tp2_tamby_5.config;
+
+/**
+ *
+ * @author tamby
+ */
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
